25e10aa59b4daef7a468729b18cb2f808449a3f6
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2024-11-30 18:27:30 -0300

    added cors

--- index.js
+++ index.js
@@ -30,16 +30,16 @@ const express_1 = __importStar(require("express"));
 const path_1 = require("path");
 const log_1 = __importDefault(require("./utils/log"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 const isDevelopment = process.env.NODE_ENV === "development";
+app.use((0, cors_1.default)());
 app.use((0, express_1.json)());
 app.use(log_1.default);
 app.use("/v1/hello", function (_, res) {
-    res
-        .status(200)
-        .json({ error: false, message: "Hello, this API Version 1 is working" });
+    res.status(200).json({ error: false, message: "Hello, this API Version 1 is working" });
 });
 const publicDir = isDevelopment ? "../public" : "public";
 app.use(express_1.default.static((0, path_1.join)(__dirname, publicDir, "assets")));
--- ts-server/index.ts
+++ ts-server/index.ts
@@ -2,20 +2,20 @@ import express, { Response, json } from "express";
 import { join } from "path";
 import logs from "./utils/log";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const isDevelopment = process.env.NODE_ENV === "development";
 
+app.use(cors());
 app.use(json());
 app.use(logs);
 
 // Api routes
 app.use("/v1/hello", function (_, res: Response) {
-  res
-    .status(200)
-    .json({ error: false, message: "Hello, this API Version 1 is working" });
+  res.status(200).json({ error: false, message: "Hello, this API Version 1 is working" });
 });
 
 // Serve static files
