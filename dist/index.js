45e8a9e8e205deeb29d5d8980260798fee39930a
heitormbonfim <heitor.mb27@gmail.com>
2025-03-11 21:15:46 -0300

    testing new listening handler for vercel

--- dist/index.js
+++ dist/index.js
@@ -36,7 +36,7 @@ dotenv_1.default.config();
 const portFlag = process.argv[2];
 const portArg = process.argv[3];
 let PORT = 5000;
-if (portFlag == "--port" && portArg) {
+if (portFlag === "--port" && portArg) {
     const portNum = Number(portArg);
     if (isNaN(portNum)) {
         throw new Error("Port must be a number");
@@ -57,26 +57,29 @@ const limiter = (0, express_rate_limit_1.default)({
 server.use(limiter);
 server.use((0, express_1.json)());
 server.use(log_1.default);
-server.use("/api/hello", function (_, res) {
+server.use("/api/hello", (_, res) => {
     res.status(200).json({ error: false, message: "Hello, this API Version 1 is working" });
 });
 exports.staticFilesPath = (0, path_1.join)(__dirname, "public", "index.html");
 server.use(express_1.default.static((0, path_1.join)(__dirname, "public")));
 server.get("*", (_, res) => res.sendFile(exports.staticFilesPath));
-server.listen(PORT, () => {
-    const env = DEVELOPMENT ? "DEVELOPMENT" : "PRODUCTION";
-    const logData = {
-        timestamp: new Date().toLocaleString("en", {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-        }),
-        message: "Server is running",
-        port: PORT,
-        mode: env,
-    };
-    console.info(logData);
-});
+if (!process.env.VERCEL) {
+    server.listen(PORT, () => {
+        const env = DEVELOPMENT ? "DEVELOPMENT" : "PRODUCTION";
+        const logData = {
+            timestamp: new Date().toLocaleString("en", {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+            }),
+            message: "Server is running",
+            port: PORT,
+            mode: env,
+        };
+        console.info(logData);
+    });
+}
+exports.default = (req, res) => server(req, res);
